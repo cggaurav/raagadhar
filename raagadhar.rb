a0aab17780b6cf7ab120bb32c0d2fd6d3ad48511
Gaurav Chandrashekar <cggaurav@gmail.com>
2012-05-30 14:26:06 +0530

    hello world

--- raagadhar.rb
+++ raagadhar.rb
@@ -6,7 +6,7 @@ if ENV['MONGOHQ_URL']
 	conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
 	db = conn.db(uri.path.gsub(/^\//, ''))
 else
-  	db = Mongo::Connection.new('localhost').db('fuelprice')
+  	db = Mongo::Connection.new('localhost').db('raagadhar')
 end
 
 get '/' do
